95b5ec2e5ae1c000a8c68b144969852859c802f3
Sebastian Ossner <github@sebastianoner.com>
2022-05-01 23:10:39 +0200

    Add encoding methods

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -128,5 +128,156 @@ export default class App extends React.Component {
 
   onChange(value) {
     this.setState({ value: value });
+    this.encodeNRZL(value)
+    this.encodeNRZM(value)
+    this.encodeNRZS(value)
+    this.encodeManchester(value)
+    this.encodeBiphaseM(value)
+    this.encodeBiphaseS(value)
+  }
+
+  encodeNRZL(value) {
+    let encoding_cp = Array(16).fill(<Empty />);
+    let level
+    for (var i = 0; i < value.length; i++) {
+      if (i === 0) {
+        if (value.charAt(i) === '1') {
+          encoding_cp[i] = <ConstOne />;
+          level = true
+        } else {
+          encoding_cp[i] = <ConstZero />;
+          level = false
+        }
+        continue
+      }
+      if (value.charAt(i) === '1' && level) { encoding_cp[i] = <ConstOne /> }
+      else if (value.charAt(i) === '0' && !level) { encoding_cp[i] = <ConstZero /> }
+      else {
+        encoding_cp[i] = level ? <TickZero /> : <TickOne />;
+        level = !level
+      }
+    }
+    this.setState({ nrzL: encoding_cp });
+  }
+
+  encodeNRZM(value) {
+    let encoding_cp = Array(16).fill(<Empty />);
+    let level
+    for (var i = 0; i < value.length; i++) {
+      if (i === 0) {
+        if (value.charAt(i) === '1') {
+          encoding_cp[i] = <ConstOne />
+          level = true
+        } else {
+          encoding_cp[i] = <ConstZero />
+          level = false
+        }
+        continue
+      }
+      if (value.charAt(i) === '1') {
+        encoding_cp[i] = level ? <TickZero /> : <TickOne />
+        level = !level
+      } else {
+        encoding_cp[i] = level ? <ConstOne /> : <ConstZero />
+      }
+    }
+    this.setState({ nrzM: encoding_cp });
+  }
+
+  encodeNRZS(value) {
+    let encoding_cp = Array(16).fill(<Empty />);
+    let level
+    for (var i = 0; i < value.length; i++) {
+      if (i === 0) {
+        if (value.charAt(i) === '1') {
+          encoding_cp[i] = <ConstOne />
+          level = true
+        } else {
+          encoding_cp[i] = <ConstZero />
+          level = false
+        }
+        continue
+      }
+      if (value.charAt(i) === '0') {
+        encoding_cp[i] = level ? <TickZero /> : <TickOne />
+        level = !level
+      } else {
+        encoding_cp[i] = level ? <ConstOne /> : <ConstZero />
+      }
+    }
+    this.setState({ nrzS: encoding_cp });
+  }
+
+  encodeManchester(value) {
+    let encoding_cp = Array(16).fill(<Empty />);
+    let level
+    for (var i = 0; i < value.length; i++) {
+      if (i === 0) {
+        if (value.charAt(i) === '1') {
+          encoding_cp[i] = <BiphaseOne />
+          level = true
+        } else {
+          encoding_cp[i] = <BiphaseZero />
+          level = false
+        }
+        continue
+      }
+      if (value.charAt(i) === '0') {
+        encoding_cp[i] = level ? <BiphaseZero /> : <TickBiphaseZero />
+        level = false
+      } else {
+        encoding_cp[i] = level ? <TickBiphaseOne /> : <BiphaseOne />
+        level = true
+      }
+    }
+    this.setState({ manchester: encoding_cp });
+  }
+
+  encodeBiphaseM(value) {
+    let encoding_cp = Array(16).fill(<Empty />);
+    let level
+    for (var i = 0; i < value.length; i++) {
+      if (i === 0) {
+        if (value.charAt(i) === '1') {
+          encoding_cp[i] = <BiphaseZero />
+          level = false
+        } else {
+          encoding_cp[i] = <ConstOne />
+          level = true
+        }
+        continue
+      }
+      if (value.charAt(i) === '1') {
+        encoding_cp[i] = level ? <TickBiphaseOne /> : <TickBiphaseZero />
+      } else {
+        encoding_cp[i] = level ? <TickZero /> : <TickOne />
+        level = !level
+      }
+    }
+    this.setState({ biphaseM: encoding_cp });
+  }
+
+  encodeBiphaseS(value) {
+    let encoding_cp = Array(16).fill(<Empty />);
+    let level
+    for (var i = 0; i < value.length; i++) {
+      if (i === 0) {
+        if (value.charAt(i) === '0') {
+          encoding_cp[i] = <BiphaseZero />
+          level = false
+        } else {
+          encoding_cp[i] = <ConstOne />
+          level = true
+        }
+        continue
+      }
+      if (value.charAt(i) === '0') {
+        encoding_cp[i] = level ? <TickBiphaseOne /> : <TickBiphaseZero />
+      } else {
+        encoding_cp[i] = level ? <TickZero /> : <TickOne />
+        level = !level
+      }
+    }
+    this.setState({ biphaseS: encoding_cp });
   }
 }
