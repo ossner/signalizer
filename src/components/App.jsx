3110eb791fd0d689cfc30c2545253c4be2c2c29d
Sebastian Ossner <github@sebastianoner.com>
2022-05-01 23:10:31 +0200

    Add encoding lines

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,9 +7,12 @@ export default class App extends React.Component {
     super(props)
     this.state = {
       value: "",
-      nrzl: Array(16).fill(<Empty />),
-      nrzm: Array(16).fill(<Empty />),
-      nrzs: Array(16).fill(<Empty />),
+      nrzL: Array(16).fill(<Empty />),
+      nrzM: Array(16).fill(<Empty />),
+      nrzS: Array(16).fill(<Empty />),
+      manchester: Array(16).fill(<Empty />),
+      biphaseM: Array(16).fill(<Empty />),
+      biphaseS: Array(16).fill(<Empty />),
     }
   }
 
@@ -31,11 +34,93 @@ export default class App extends React.Component {
             Data/NRZ-L:
           </div>
           <div style={{
-            border: "1px solid white",
+            border: "1px solid #f8f8f2",
             gridColumn: 2,
-            gridRow: 2
+            gridRow: 2,
+            display: "grid",
+            gridTemplateColumns: "repeat(16, 1fr)"
           }}>
-            <Empty />
+            <Encoding data={this.state.nrzL} />
+          </div>
+          <div style={{
+            textAlign: "right", color: "#f8f8f2", lineHeight: "65px", fontSize: 30,
+            gridColumn: 1,
+            gridRow: 3
+          }}>
+            NRZ-M:
+          </div>
+          <div style={{
+            border: "1px solid #f8f8f2",
+            gridColumn: 2,
+            gridRow: 3,
+            display: "grid",
+            gridTemplateColumns: "repeat(16, 1fr)"
+          }}>
+            <Encoding data={this.state.nrzM} />
+          </div>
+          <div style={{
+            textAlign: "right", color: "#f8f8f2", lineHeight: "65px", fontSize: 30,
+            gridColumn: 1,
+            gridRow: 4
+          }}>
+            NRZ-S:
+          </div>
+          <div style={{
+            border: "1px solid #f8f8f2",
+            gridColumn: 2,
+            gridRow: 4,
+            display: "grid",
+            gridTemplateColumns: "repeat(16, 1fr)"
+          }}>
+            <Encoding data={this.state.nrzS} />
+          </div>
+          <div style={{
+            textAlign: "right", color: "#f8f8f2", lineHeight: "65px", fontSize: 30,
+            gridColumn: 1,
+            gridRow: 5
+          }}>
+            Manchester:
+          </div>
+          <div style={{
+            border: "1px solid #f8f8f2",
+            gridColumn: 2,
+            gridRow: 5,
+            display: "grid",
+            gridTemplateColumns: "repeat(16, 1fr)"
+          }}>
+            <Encoding data={this.state.manchester} />
+          </div>
+          <div style={{
+            textAlign: "right", color: "#f8f8f2", lineHeight: "65px", fontSize: 30,
+            gridColumn: 1,
+            gridRow: 6
+          }}>
+            Biphase-M:
+          </div>
+          <div style={{
+            border: "1px solid #f8f8f2",
+            gridColumn: 2,
+            gridRow: 6,
+            display: "grid",
+            gridTemplateColumns: "repeat(16, 1fr)"
+          }}>
+            <Encoding data={this.state.biphaseM} />
+          </div>
+          <div style={{
+            textAlign: "right", color: "#f8f8f2", lineHeight: "65px", fontSize: 30,
+            gridColumn: 1,
+            gridRow: 7
+          }}>
+            Biphase-S:
+          </div>
+          <div style={{
+            border: "1px solid #f8f8f2",
+            gridColumn: 2,
+            gridRow: 7,
+            display: "grid",
+            gridTemplateColumns: "repeat(16, 1fr)"
+          }}>
+            <Encoding data={this.state.biphaseS} />
           </div>
         </div>
       </div>);
